56ce2708570ffa4139e1a3970d1be1d48ff6b8b4
Luke LaBonte <lukesterlab@gmail.com>
2024-02-29 10:13:16 -0500

    Implement tickets 5-9

--- applicationManager.js
+++ applicationManager.js
@@ -1,43 +1,121 @@
 
 const GLOBAL_CONFIG = {
 	parent: null,
-	"CustomerPanel": {
-		"title": "Customers",
-		"table": "customers",
-		"related": [{
-			"title": "Orders",
-			"typeToOpen": "OrderPanel",
-			"primaryKey": "id",
-			"foreignKey": "CustomerID"
-		}]
-	},
-	"OrderPanel": {
-		"title": "Orders",
-		"table": "orders",
-		"related": [
+	parentObjName: "",
+	CustomerPanel: {
+		title: "Customers",
+		table: "customers",
+		serverSearch: false,
+		related: [{
+			title: "Orders",
+			typeToOpen: "OrderPanel",
+			primaryKey: "id",
+			foreignKey: "CustomerID"
+		},
+		{
+			title: "Static Panel",
+			overrideAction: "openStaticPanel",
+		}],
+		mappings: [
 			{
-				"title": "Products",
-				"typeToOpen": "ProductPanel",
-				"primaryKey": "ProductID",
-				"foreignKey": "id"
+				columnName: "id",
+				inList: false,
+				inDetailView: false,
 			},
 			{
-				"title": "Customers",
-				"typeToOpen": "CustomerPanel",
-				"primaryKey": "CustomerID",
-				"foreignKey": "id"
+				columnName: "FirstName",
+				displayName: "First Name",
+				inList: true,
+				inDetailView: true,
+			},
+			{
+				columnName: "LastName",
+				displayName: "Last Name",
+				inList: true,
+				inDetailView: true,
+			},
+			{
+				columnName: "Email",
+				displayName: "Email",
+				inList: true,
+				inDetailView: true,
 			},
 		]
 	},
-	"ProductPanel": {
-		"title": "Products",
-		"table": "products",
-		"related": [{
-			"title": "Orders",
-			"typeToOpen": "OrderPanel",
-			"primaryKey": "id",
-			"foreignKey": "ProductID"
-		}]
+	OrderPanel: {
+		title: "Orders",
+		table: "orders",
+		serverSearch: false,
+		related: [
+			{
+				title: "Products",
+				typeToOpen: "ProductPanel",
+				primaryKey: "ProductID",
+				foreignKey: "id"
+			},
+			{
+				title: "Customers",
+				typeToOpen: "CustomerPanel",
+				primaryKey: "CustomerID",
+				foreignKey: "id"
+			},
+		],
+		mappings: [
+			{
+				columnName: "id",
+				inList: false,
+				inDetailView: false,
+			},
+			{
+				columnName: "Quantity",
+				inList: true,
+				inDetailView: true,
+			},
+			{
+				columnName: "OrderDate",
+				displayName: "Order Date",
+				inList: true,
+				inDetailView: true,
+				editType: 'datetime',
+			}
+		]
+	},
+	ProductPanel: {
+		title: "Products",
+		table: "products",
+		serverSearch: true,
+		related: [{
+			title: "Orders",
+			typeToOpen: "OrderPanel",
+			primaryKey: "id",
+			foreignKey: "ProductID"
+		}],
+		mappings: [
+			{
+				columnName: "id",
+				inList: false,
+				inDetailView: false,
+			},
+			{
+				columnName: "ProductName",
+				displayName: "Product Name",
+				inList: true,
+				inDetailView: true,
+			},
+			{
+				columnName: "WholesaleCost",
+				displayName: "Wholesale Cost",
+				inList: true,
+				inDetailView: true,
+				template: '<div style="display: table-cell; vertical-align: middle; horizontal-align: middle;">${WholesaleCost}</div>'
+			},
+			{
+				columnName: "RetailPrice",
+				displayName: "Retail Price",
+				inList: true,
+				inDetailView: true,
+			}
+		]
 	},
 }
 
